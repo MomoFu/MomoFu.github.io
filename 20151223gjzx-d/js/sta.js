7dedea38231a72ed4f07e0770ba2ecf2bbb6afd7
MomoFu <qiang.fu@cootek.cn>
2015-12-28 18:55:54 +0800

    change some function for app

---
+++ 20151223gjzx-d/js/sta.js
@@ -0,0 +1,9 @@
+
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "//hm.baidu.com/hm.js?f6200b3f84a33687e8caa711f5772fb8";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
+
